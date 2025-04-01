c95105a6e048f6c0e858ee6ea5c88e3d4eb577d0
ZestyZach2000 <lightningalpha007@gmail.com>
2025-04-01 10:17:55 -0700

    Create File-lab#1.py

---
+++ File-lab#1.py
@@ -0,0 +1,6 @@
+xfile = open("demofile.txt", "r")
+for cheese in xfile:
+    print(cheese)
+
+#https://mail.google.com/mail/u/0?ui=2&ik=f156c2a166&attid=0.1&permmsgid=msg-a:r5883785447792042860&th=195f258989577ef5&view=att&disp=safe&realattid=f_m8yrcc770&zw
+#copy this https then name it demofile.txt
